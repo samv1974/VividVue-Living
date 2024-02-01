854f85c6552d9aa2241942fce26713cefd3d4083
samv1974 <svasishat2003@gmail.com>
2024-02-01 21:40:45 +0530

    commit sign-up ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { BrowserRouter,Routes,Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Signin from './pages/signin'
-import SignOut from './pages/signOut'
+import SignOut from './pages/SignUp'
 import Profile from './pages/profile'
 import About from './pages/about'
 import Header from './Components/header'
@@ -16,7 +16,7 @@ export default function App() {
       <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/sign-in' element={<Signin/>}/>
-      <Route path='/sign-out' element={<SignOut/>}/>
+      <Route path='/sign-up' element={<SignOut/>}/>
       <Route path='/profile' element={<Profile/>}/>
       <Route path='/about' element={<About/>}/>
       </Routes>
