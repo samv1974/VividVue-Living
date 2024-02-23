532c3c69c1ef0dee5523a1413a32dac3bd2ec212
samv1974 <svasishat2003@gmail.com>
2024-02-23 23:45:47 +0530

    create user update api

--- api/index.js
+++ api/index.js
@@ -3,6 +3,8 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
+import cookieParser from 'cookie-parser'
+
 // initialize dotenv
 dotenv.config();
 
@@ -14,6 +16,8 @@ mongoose.connect(process.env.Mongo,{ useNewUrlParser: true, useUnifiedTopology: 
 // created the server and running the server on port 3000
 const app = express();
 app.use(express.json());
+
+app.use(cookieParser());
 
 app.listen(3000,() => {
     console.log('server is running on port 3000!!!!')
